935962004c75bfc450e4c124b9a4c0e625b89d92
Jaime Carrera <dongraft@gmail.com>
2010-12-05 00:30:47 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LectureWeb::Application.config.secret_token = 'efd51cf9821bb3508cdcfd2d5d33f6e56d8323465d93d778c063747712504c0e52918e76a33c3e7d39b9051c05c4590cba012f4a5c4e14ab57653606f4556ecd'
